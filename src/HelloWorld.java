89045a65518d8a73323f25b503c7d3e41c2eb7be
manharpuri <manhar.puri@accenture.com>
2017-02-08 08:27:14 +0000

    modified helloworld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main(String args[]) {
-		System.out.println("Hello World");
+		String name = "Manhar Puri";
+		System.out.println("Hello World " + name);
 	}
 }
